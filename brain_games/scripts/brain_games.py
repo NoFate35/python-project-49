b5ff97d2a217040e25c2f62aa77cc5be56baebdb
NoFate35 <vsokol35@mail.ru>
2023-02-17 10:22:47 +0300

    remove the errors that 'flake8' give out in files 'brain_games/cli.py', 'brain_games/scripts/brain_games.py '; add into Makefile 'make lint: poetry run flake8 brain_games'

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-import  brain_games.cli
+import brain_games.cli
+
 
 def greeting():
-	print('Welcome to the Brain Games!')
-
+    print('Welcome to the Brain Games!')
 
 
 def main():
-	greeting()
-	brain_games.cli.welcome_user()
+    greeting()
+    brain_games.cli.welcome_user()
+
 
 if __name__ == '__main__':
-	main()
+    main()
